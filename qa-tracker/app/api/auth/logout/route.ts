4734c12a02d43c541b12e6f37b626c5cb55db3c3
Anusha <anusha@marensolutions.com>
2025-06-26 19:27:42 +0530

    console remove

--- qa-tracker/app/api/auth/logout/route.ts
+++ qa-tracker/app/api/auth/logout/route.ts
@@ -2,13 +2,13 @@ import { NextResponse } from "next/server";
 
 export async function POST() {
   const res = NextResponse.json({ message: "Logged out" });
-  console.log("Logout API hit"); 
+ 
 
   res.cookies.set("token", "", {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
     path: "/",
-    expires: new Date(0), // Immediately expire the token
+    expires: new Date(0), 
   });
 
   return res;
