a761cdd20f5faa0e629f12a00284dcbc94b77710
govind bhatt <govind@marensolutions.com>
2025-06-27 11:50:00 +0530

    loader added

---
+++ qa-tracker/app/common/loader.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function Loader() {
+  return (
+    <div className="flex items-center justify-center h-screen fixed left-0 right-0 bg-[rgba(232,243,252,0.5)] z-[11] top-0">
+     <div className="h-8 w-8 animate-spin rounded-full border-4 border-[#1F66A0] border-t-transparent"></div>
+     </div>
+  );
+}
